27179fde3d14999200a92be6a648007de1732818
rati <rati@oac.ge>
2021-06-04 17:11:17 +0400

    in progress

---
+++ app/Http/Controllers/StripeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Stripe;
+use Session;
+
+class StripeController extends Controller
+{
+    /**
+     * payment view
+     */
+    public function handleGet()
+    {
+        return view('stripe');
+    }
+
+    /**
+     * handling payment with POST
+     */
+    public function handlePost(Request $request)
+    {
+        Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+          $response = Stripe\Charge::create ([
+                  "amount" => 100 * 200,
+                  "currency" => "inr",
+                  "source" => $request->stripeToken,
+                  "description" => "Making test payment of 200 ruppes."
+          ]);
+
+      dd($response);
+
+
+        //$respomse obj if paid set to 1
+        Session::flash('success', 'Payment has been successfully processed.');
+
+        return back();
+    }
+}
---
+++ resources/views/dashboard/shared/nav.blade.php
@@ -0,0 +1,17 @@
+      <div class="c-sidebar-brand"><img class="c-sidebar-brand-full" src="/assets/brand/coreui-base-white.svg" width="118" height="46" alt="CoreUI Logo"><img class="c-sidebar-brand-minimized" src="assets/brand/coreui-signet-white.svg" width="118" height="46" alt="CoreUI Logo"></div>
+      <nav class="c-sidebar-nav">
+        <ul class="c-nav">
+          <li class="c-nav-item"><a class="c-nav-link" href="/">
+ Dashboard<span class="badge badge-info">NEW</span></a></li>
+              <li class="c-nav-item"><a class="c-nav-link" href="/login" target="_top">
+Login</a></li>
+              <li class="c-nav-item"><a class="c-nav-link" href="/register" target="_top">
+ Register</a></li>
+          <li class="c-nav-item mt-auto"><a class="c-nav-link c-nav-link-success" href="https://coreui.io" target="_top">
+ Download CoreUI</a></li>
+          <li class="c-nav-item"><a class="c-nav-link c-nav-link-danger" href="https://coreui.io/pro/" target="_top">
+Try CoreUI<strong>PRO</strong></a></li>
+        </ul>
+      </nav>
+      <button class="c-sidebar-minimizer c-class-toggler" type="button" data-target="_parent" data-class="c-sidebar-minimized"></button>
+    </div>
